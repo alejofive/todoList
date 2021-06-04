ed435b311bb78917df3b97ec364ad5ff910d968b
AlejoFive <alejofive555@gmail.com>
2021-06-04 08:23:54 -0500

    vercion 2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import React from 'react'
 import './App.css';
+import { FaEdit } from 'react-icons/fa';
+
 
 const data = [
-  { Nombre: 'Alejandro', },
-  { Nombre: 'Maria', },
-  { Nombre: 'Pedro', },
-  { Nombre: 'Luis', },
-  { Nombre: 'Julio', },
-  { Nombre: 'Sidney', }
+  { id: 0, Nombre: 'Alejandro', },
+  { id: 1, Nombre: 'Maria', },
+  { id: 2, Nombre: 'Pedro', },
+  { id: 3, Nombre: 'Luis', },
+  { id: 4, Nombre: 'Julio', },
+  { id: 5, Nombre: 'Sidney', }
 ]
 
 class App extends React.Component {
   state = {
     data: data,
-    from: {
+    form: {
       Nombre: '',
-    }
+      id: '',
+    },
+    formEditar: {
+      Nombre: '',
+      id: '',
+    },
+    posicion: -1
   }
 
-  handlechge = e => {
+  handleChange = e => {
     this.setState({
       form: {
         ...this.state.form,
@@ -27,12 +35,21 @@ class App extends React.Component {
     })
   }
 
-  insertar = () => {
+  insertar = (e) => {
+    e.preventDefault();
+    if (this.state.form.Nombre.length == '') {
+      alert('no puede ir vacio');
+      return;
+    }
     var valorNuevo = { ...this.state.form };
     var lista = this.state.data;
-    lista.push(valorNuevo)
-    this.setState({ data: lista })
-  }
+    lista.push(valorNuevo);
+    this.setState({ data: lista });
+    this.setState({ form: { Nombre: '' } });
+
+  };
+
+
 
   eliminar = (dato) => {
     var opcion = window.confirm('realmente desea Eliminar ' + dato.Nombre);
@@ -49,27 +66,82 @@ class App extends React.Component {
     }
   }
 
+  editar = (elemento) => {
+    this.setState({ posicion: elemento.id, formEditar: { ...elemento } })
+    console.log(this.state.formEditar)
+    console.log(elemento)
+  }
+
+  cancelar = () => {
+    this.setState({ posicion: - 1 })
+  }
+
+  handlEdit = (e) => {
+    this.setState({
+      formEditar: {
+        ...this.state.form,
+        [e.target.name]: e.target.value,
+      }
+    })
+  }
+
+  aceptar = () => {
+
+    let dataNueva = [
+      ...this.state.data.map(elemento => {
+        if (elemento.id == this.state.posicion) {
+          return {
+            id: elemento.id,
+            Nombre: this.state.formEditar.Nombre
+          }
+        }
+        return elemento
+      })
+    ]
+    this.setState({ data: dataNueva })
+  }
+
+
   render() {
     return (
       <div className='center'>
         <section className='caja-big'>
           <div className='title'>
-            <h1>Personas </h1>
+            <h1>Personas</h1>
           </div>
 
           <div>
-            {this.state.data.map((elemento) => (
-              <div className='caja-list'>
-                <p className='list-nombre'>{elemento.Nombre}</p>
-                <button onClick={() => this.eliminar(elemento)} >x</button>
-              </div>
-            ))}
+            {this.state.data.map((elemento) => {
+              if (elemento.id != this.state.posicion) {
+                return (
+                  <div className='caja-list'>
+                    <p className='list-nombre'>{elemento.Nombre}</p>
+                    <div className='botones'>
+                      <button className='edit' onClick={() => this.editar(elemento)}><FaEdit /></button>
+                      <button className='eliminar' onClick={() => this.eliminar(elemento)} >x</button>
+                    </div>
+                  </div>
+                )
+              } else {
+                return (<div className='caja-list caja-list-edit'>
+                  <input type="text" placeholder='' name='Nombre' value={this.state.formEditar.Nombre} onChange={(e) => this.handlEdit(e)} />
+                  <div className='botones-edit'>
+                    <button className='Aceptar' onClick={() => this.aceptar()} >Aceptar</button>
+                    <button onClick={() => this.cancelar()} className='Cancelar'>Cancelar</button>
+                  </div>
+                </div>)
+              }
+            }
+            )}
           </div>
 
-          <div className='agregar'>
-            <input type="text" name='Nombre' onChange={this.handlechge} placeholder='Ingrese un Nombre' />
-            <button onClick={() => this.insertar()} >Sumit</button>
-          </div>
+
+
+          <form className="agregar" onSubmit={this.insertar}>
+            <input type="text" name="Nombre" onChange={this.handleChange} placeholder="Ingrese un Nombre" value={this.state.form.Nombre} />
+            <button type="submit">Submit</button>
+          </form>
+
 
         </section>
       </div >
